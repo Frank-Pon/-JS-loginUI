8604673585ee2fa53c4f899bb129bf16a81e2d6d
Frank-Pon <cry133216@gmail.com>
2025-04-01 15:42:58 +0800

    使用者登入介面

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let is_Error = false;
 let log_success = false;
 
 function click(){
+    
+
     if(log_success){
         account.style.display = 'block';
         password.style.display = 'block';
@@ -25,15 +27,29 @@ function click(){
     }
     
     if(account.value == user.username && password.value == user.pw){
-        result.innerHTML = `${user.username} 登入成功!<br>歡迎您`;
-        title.innerText = '登入成功';
-        title.style.color = 'green';
-        result.style.color = 'green';
-        account.style.display = 'none';
-        password.style.display = 'none';
-        butt.innerText = '登出';
-        log_success = true;
-    }else if (account.value == '' || password.value == ''){
+        let dotcount = 0;
+        result.style.color = 'grey';
+        result.innerText = '登入中';
+        let loadinginterval = setInterval(()=>{
+            dotcount = (dotcount+1) % 4;
+            let dots='.'.repeat(dotcount);
+            result.innerText = '登入中'+dots;
+        },375);
+        setTimeout(() => {
+            clearInterval(loadinginterval);
+            dots= '';
+            dotcount = 0; 
+            result.innerHTML = `${user.username} 登入成功!<br>歡迎您`;
+            title.innerText = '登入成功';
+            title.style.color = 'green';
+            result.style.color = 'green';
+            account.style.display = 'none';
+            password.style.display = 'none';
+            butt.innerText = '登出';
+            log_success = true;
+        }, 1500);
+    }
+    else if (account.value == '' || password.value == ''){
         result.innerText = '帳號、密碼不可空白';
         result.style.color = 'red';
     }
